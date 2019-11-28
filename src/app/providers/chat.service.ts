75dbefb0f4774f8b1eb6727fce4b6b3d003f6761
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-28 12:19:27 +0100

    fin

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -3,6 +3,8 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Mensaje } from '../interface/mensaje.interface';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,18 @@ export class ChatService {
 
   public chats: Mensaje[] = [];
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  public usuario: any = {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    // Nos suscribimos a este metodo para ecuchar cualquier cambio en el estado de la autenticacion.
+    this.afAuth.authState.subscribe(user => {
+      console.log('Estado usuario', user);
+      if (!user) { return; }
+
+      this.usuario.nombre = user.displayName;
+      this.usuario.uid = user.uid;
+    });
+  }
 
   cargarMensajes(): Observable<any> {
     // Obtengo los ultmos 5 pero descendente. Luego ordenamos por JS
@@ -34,11 +46,22 @@ export class ChatService {
 
   agregarMensaje(texto: string): Promise<any> {
     const mensaje: Mensaje = {
-      nombre: 'Roberto prueba',
+      nombre: this.usuario.nombre,
       mensaje: texto,
       fecha: new Date().getTime(),
+      uid: this.usuario.uid
     };
     return this.itemsCollection.add(mensaje);
+  }
+
+  login(proveedor: string) {
+    // Solo con google ya que no hay acceso a Twitter
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.usuario = {};
+    this.afAuth.auth.signOut();
   }
 
 }
